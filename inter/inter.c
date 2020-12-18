5206c8061a3ec06dc504ea542642631aca560fa3
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-12-18 09:28:42 +0900

    [Improve] fin inter.c

--- inter/inter.c
+++ inter/inter.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-void ft_putstr(char *s)
+void ft_inter(char *str1, char *str2)
 {
-	while (*s)
-		write(1, s++, 1);
+	int i = 0;
+	int ascii[256] = {0};
+
+	while (str2[i])
+	{
+		if (ascii[(int)str2[i]] == 0)
+			ascii[(int)str2[i]] = 1;
+		i++;
+	}
+
+	i = 0;
+	while (str1[i])
+	{
+		if (ascii[(int)str1[i]] == 1)
+		{
+			ascii[(int)str1[i]] = 2;
+			write(1, &str1[i], 1);
+		}
+		i++;
+	}
 }
 
 int main(int ac, char **av)
 {
-	
+	if (ac == 3)
+		ft_inter(av[1], av[2]);
+	return (write(1, "\n", 1));
 }
