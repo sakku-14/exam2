7b44efe95c2ac5f1475516b7e0f5d6ab099ad728
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2020-12-18 11:41:49 +0900

    a

--- union/union.c
+++ union/union.c
@@ -1,6 +1,30 @@
 #include <unistd.h>
 
-int main()
+void ft_union(char *first, char *second)
 {
-	
+	int i = 0;
+	int ascii[256] = {0};
+
+	while (first[i])
+	{
+		if (ascii[(int)first[i]] == 0)
+			ascii[(int)first[i]] = 1;
+		i++;
+	}
+	i = 0;
+	while (first[i])
+	{
+		if (ascii[(int)first[i]] == 1)
+		{
+			
+			write(1, &first[i], 1);
+		}
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 3)
+		ft_union(av[1], av[2]);
+	return (write(1, "\n", 1));
 }
